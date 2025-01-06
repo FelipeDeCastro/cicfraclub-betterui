b09e841a70d412f97523addddd250ccfd79cb7c3
FelieDeCastro <felipepvc@gmail.com>
2025-01-06 14:37:58 +0000

    Center align paper sheets and set default checkbox states: two columns true and tablatures false

--- content-script.js
+++ content-script.js
@@ -7,10 +7,23 @@ window.addEventListener('load', () => {
       gap: 1rem;
       margin: 0 1rem;
       flex-wrap: wrap;
+      justify-content: center;
     }
     .folha {
-        margin: 0;
+      margin: 0;
     }
   `;
   document.head.appendChild(style);
+
+  const duasColunasCheckbox = document.querySelector('#exib1');
+  const tablaturasCheckbox = document.querySelector('#exib4');
+
+  if (duasColunasCheckbox) {
+    duasColunasCheckbox.checked = true;
+    duasColunasCheckbox.dispatchEvent(new Event('change'));
+  }
+  if (tablaturasCheckbox) {
+    tablaturasCheckbox.checked = false;
+    tablaturasCheckbox.dispatchEvent(new Event('change'));
+  }
 });
